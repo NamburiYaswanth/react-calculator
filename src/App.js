37b1a8901d93971ecad21847ea264a0f95ff4ee0
yaswanthy <yaswanthnamburi7@gmail.com>
2024-07-25 10:28:58 +0530

    calculator-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
+export const CALCULATOR_TITLE = "Calculator";
 
-function App() {
+const INPUTS = [
+  [7, 8, 9, "%"],
+  [4, 5, 6, "*"],
+  [1, 2, 3, "-"],
+  [0, ".", "+", "="],
+];
+
+export default class Calculator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+    };
+  }
+
+  clickHandler = (event) => {
+    const input = event.target.innerHTML;
+    let newResult = this.state.result;
+    if (input === "=") {
+      newResult = eval(newResult);
+    } else {
+      newResult += input;
+    }
+    this.setState({ result: newResult });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{CALCULATOR_TITLE}</h2>
+        <div className="calc">
+          <div className="display">{this.state.result}</div>
+          {INPUTS.map((inputArray) => (
+            <div className="row">
+              {inputArray.map((input) => (
+                <button onClick={this.clickHandler}>{input}</button>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export function CalculatorFn() {
+  const [result, setResult] = useState("");
+
+  const clickHandler = (event) => {
+    const input = event.target.innerHTML;
+    let newResult = result;
+    if (input === "=") {
+      newResult = eval(newResult);
+    } else {
+      newResult += input;
+    }
+    setResult(newResult);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{CALCULATOR_TITLE}</h2>
+      <div className="calc">
+        <div className="display">{result}</div>
+        {INPUTS.map((inputArray) => (
+          <div className="row">
+            {inputArray.map((input) => (
+              <button onClick={clickHandler}>{input}</button>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
